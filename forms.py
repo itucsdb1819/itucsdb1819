b76fb7464648b7d9bcf53bbb2002f17655d5bc6e
yigitKomsu <ihsanyigitergin@gmail.com>
2018-12-10 21:45:24 +0300

    even more fixes

--- forms.py
+++ forms.py
@@ -193,7 +193,7 @@ class Localization:
             AND LocaleID = '{}'""".format(resourceID, resourceSet, localeID)
         cursor.execute(queryString)
         if cursor.rowcount == 0:
-            return tuple()
+            return ('')
         value = cursor.fetchone()
         return value
 
