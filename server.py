04e5a99fa28eff2788e9d9e31d6a0e6f99c65379
yigitKomsu <ihsanyigitergin@gmail.com>
2018-12-11 00:04:35 +0300

    fixed deployment issue

--- server.py
+++ server.py
@@ -87,8 +87,8 @@ def roles_and_permissions_page():
                 redirect(url_for('roles_and_permissions'))
             elif request.form['submit_button'] == 'selectRole':
                 selectedRoleId = request.form.get('selectedRole')
-        selectedRole = forms.Role.selectWithID(selectedRoleId)
         else:
+            selectedRole = forms.Role.selectWithID(selectedRoleId)
             rolesAndPermissions = forms.RolePermission.selectRolePermissions(selectedRoleId)
             return render_template('roles_and_permissions.html', menuItems = menuItems, load_resource = load_resource, selectedRole = selectedRole, rolesAndPermissions = rolesAndPermissions, roles = roles)
     else:
