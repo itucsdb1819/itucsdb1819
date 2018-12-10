f6d49f559767cdfb2a8b5cbb8036f4f29c788cf6
yigitKomsu <ihsanyigitergin@gmail.com>
2018-12-10 23:12:35 +0300

    Added roles and permissions to the page variables

--- server.py
+++ server.py
@@ -83,7 +83,7 @@ def roles_and_permissions_page():
             selectedRoleId = request.form.get('selectedRole')
         selectedRole = forms.Role.selectWithID(selectedRoleId)
         rolesAndPermissions = forms.RolePermission.selectRolePermissions(selectedRoleId)
-    return render_template('roles_and_permissions.html', menuItems = menuItems, load_resource = load_resource, selectedRole = selectedRole, roles = roles)
+    return render_template('roles_and_permissions.html', menuItems = menuItems, load_resource = load_resource, selectedRole = selectedRole, rolesAndPermissions = rolesAndPermissions, roles = roles)
 
     return redirect(url_for('login'))
 @app.route("/roles_and_permissions_update", methods=['POST'])
